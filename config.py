e6ca0ead90d77ece817456d008dc7cb19ecd204b
HuntingBots-Team <89862218+HuntingBots-Team@users.noreply.github.com>
2025-03-09 23:02:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
-BOT_TOKEN = "1977831577:AAHVq-heDQVJiDQ8Ee6MqxtZ5JSVVMT0MIE" # Enter your telegram bot token
-OWNER_ID = "1606221784" # Enter your telegram user id
-TELEGRAM_API = "3975570" # Enter telegram API key
-TELEGRAM_HASH = "680b62f2844aa1954216f6cb99d2f3d9" # Enter telegram API hash
+BOT_TOKEN = "" # Enter your telegram bot token
+OWNER_ID = "" # Enter your telegram user id
+TELEGRAM_API = "" # Enter telegram API key
+TELEGRAM_HASH = "" # Enter telegram API hash
 
-# OPTIONAL CONFIG
